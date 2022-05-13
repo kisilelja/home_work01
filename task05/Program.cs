f321ada624e3c7b17a6ad94c02d9b3a3defceac3
Olga <o.o.kiseleva@yandex.ru>
2022-05-13 21:07:22 +0300

    add task05

---
+++ task05/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int result = 0;
+if (a > 99 && a < 1000)
+{
+    result = (a / 10) % 10;
+    Console.Write ("Вторая цифра 3хзначного числа: ");
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine ("Это не трехзначное число!");
+}
