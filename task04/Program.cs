7777c8052b9c5bdb667e6880e12d4cdd76867791
Olga <o.o.kiseleva@yandex.ru>
2022-05-13 20:37:54 +0300

    add task04

---
+++ task04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int min = 2;
+while (min <= N)
+{
+    Console.Write(min);
+    Console.Write(", ");
+    min = min + 2 ;
+}
