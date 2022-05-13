072e5085197c97a2914fbc3313bff612cd57b429
Olga <o.o.kiseleva@yandex.ru>
2022-05-13 20:10:34 +0300

    add task02

---
+++ task02/Program.cs
@@ -0,0 +1,35 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("This is Monday");
+} 
+if (a == 2)
+{
+    Console.WriteLine("This is Tuesday");
+}
+if (a == 3)
+{
+    Console.WriteLine("This is Wednesday");
+} 
+if (a == 4)
+{
+    Console.WriteLine("This is Thursday");
+}
+if (a == 5)
+{
+    Console.WriteLine("This is Friday");
+}
+if (a == 6)
+{
+    Console.WriteLine("This is Saturday");
+} 
+if (a == 7)
+{
+    Console.WriteLine("This is Sunday");
+}
+else 
+{
+    Console.WriteLine("There is no such day of the week!");
+}
