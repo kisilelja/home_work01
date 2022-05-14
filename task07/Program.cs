14655ba7bd1deb0b7f284000d1731fcaa84a5171
Olga <o.o.kiseleva@yandex.ru>
2022-05-14 22:11:38 +0300

    add task07

---
+++ task07/Program.cs
@@ -0,0 +1,14 @@
+﻿//Удалить вторую цифру трехзначного числа. 
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int firstDigit = a / 10 / 10;
+int secondDigit = (a/10) % 10;
+int thirdDigit = a % 10;
+if (a <= 999 && a >= 100)
+{
+    Console.Write($"Полученное число = {firstDigit}{thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Увы, это не трех значное число!");
+}
