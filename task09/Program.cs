ed14b55c63112a43d15ce70b5d21811968370d54
Olga <o.o.kiseleva@yandex.ru>
2022-05-14 23:49:18 +0300

    add task09

---
+++ task09/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти третью цифру числа или сообщить, что её нет 
+// Был взят определенный диапозон чисел
+int Number = new Random().Next(1, 2000);
+Console.WriteLine($"Дано число: {Number}");
+
+if (Number  > 99 && Number < 1000 ) 
+{
+    
+    Console.WriteLine($"Третья цифра: {Number % 10}");
+}
+if (Number > 1000 ) 
+{
+    
+    Console.WriteLine($"Третья цифра: {Number / 10 % 10}");
+}
+if (Number < 100)
+{
+    Console.WriteLine("У числа нет третьей цифры!");
+}
